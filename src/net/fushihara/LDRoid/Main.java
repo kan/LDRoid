77ea1fc64edb0a4ac1a2679cd373d55cf3f5f4c2
gaeeyo <iknnofm0126@gmail.com>
2010-07-30 01:26:28 +0900

    Adapterを実装してフィード一覧の見た目を変更。

--- src/net/fushihara/LDRoid/Main.java
+++ src/net/fushihara/LDRoid/Main.java
@@ -10,16 +10,21 @@ import java.util.List;
 
 import net.fushihara.LDRoid.LDRClient.Subscribe;
 import android.app.ListActivity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Main extends ListActivity {
@@ -35,6 +40,7 @@ public class Main extends ListActivity {
 
 	private List<Subscribe> subs;
 	private boolean isSubsSaved = false;
+	private int [] rateColors;
 
 	private LDRClient client;
 
@@ -44,6 +50,16 @@ public class Main extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+        Resources res = getResources();
+        rateColors = new int [] {
+        		res.getColor(R.color.rate0),
+        		res.getColor(R.color.rate1),
+        		res.getColor(R.color.rate2),
+        		res.getColor(R.color.rate3),
+        		res.getColor(R.color.rate4),
+        		res.getColor(R.color.rate5),
+        };
+        
         // 保存されている subs をセット
         setSubs(loadSubsFromFile());
     }
@@ -94,16 +110,12 @@ public class Main extends ListActivity {
 	private void setSubs(List<Subscribe> newSubs) {
 		
 		subs = newSubs;
-		List<String> items = new ArrayList<String>();
-		for (Subscribe sub: subs) {
-			items.add(sub.title + " (" + sub.unread_count + ")" );
-		}
 		
-		ArrayAdapter<String> notes = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-	    // ListActivityにアイテムリストをセットする
-	    setListAdapter(notes);
+		SubsAdapter adapter = new SubsAdapter(subs);
+		setListAdapter(adapter);
 	}
-		private LDRClientAccount getAccount() {
+	
+	private LDRClientAccount getAccount() {
 		SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
 		LDRClientAccount account = new LDRClientAccount(
 				pref.getString("login_id", null),
@@ -227,5 +239,57 @@ public class Main extends ListActivity {
         i.putExtra(KEY_SUBS_ID, sub.subscribe_id);
         i.putExtra(KEY_SUBS_TITLE, sub.title);
         startActivity(i);
+    }
+    
+    private class SubsAdapter extends BaseAdapter {
+    	private List<Subscribe> items;
+    	private LayoutInflater inflater;
+    	
+    	public SubsAdapter(List<Subscribe> subs) {
+    		items = subs;
+    		inflater = (LayoutInflater)getSystemService(
+    				Context.LAYOUT_INFLATER_SERVICE);
+    	}
+    	
+		@Override
+		public int getCount() {
+			return items.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return items.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view = convertView;
+			if (view == null) {
+				view = inflater.inflate(R.layout.subs_row, null);
+			}
+			Subscribe s = items.get(position);
+			
+			TextView t = (TextView)view.findViewById(R.id.title);
+			t.setText(s.title);
+			
+			t = (TextView)view.findViewById(R.id.count);
+			t.setText(Integer.toString(s.unread_count));
+			
+			t = (TextView)view.findViewById(R.id.ratebar);
+			if (s.rate >= 0 && s.rate <= 5) {
+				t.setBackgroundColor(rateColors[s.rate]);
+			}
+			else {
+				t.setBackgroundColor(rateColors[0]);
+			}
+
+			return view;
+		}
+    
     }
 }
